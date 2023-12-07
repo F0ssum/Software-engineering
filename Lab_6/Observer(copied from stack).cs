a214b52a25f79d0351dfa7ad40a87f1cebe05fba
Stas <ctac2710@mail.ru>
2023-12-07 20:46:04 +0300

    Lab_6 added (copied from stack), not finished

---
+++ Lab_6/Observer(copied from stack).cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+   using System;
+
+namespace Observer
+{
+    public class Input
+    {
+        private readonly IList<IObserver> observers = new List<IObserver>();
+
+        public void AddObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        public void Notify(string command)
+        {
+            for (var i = 0; i < observers.Count; i++)
+            {
+                observers[i].OnInput(command);
+            }
+        }
+    }
+
+    public interface IObserver
+    {
+        void OnInput(string command);
+    }
+
+    public class ConsoleObserver : IObserver
+    {
+        public void OnInput(string command)
+        {
+            Console.WriteLine("Команда: {0}", command);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = new Input();
+            input.AddObserver(new ConsoleObserver());
+
+            input.Notify("exit");
+        }
+    }
+}
+
+}
---
+++ Lab_6/Programm.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_6
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+        Console.WriteLine("Введите команду:");
+        string command = Console.ReadLine();
+
+        if (command == "определённая команда")
+            {
+            Console.WriteLine("Вы ввели определённую команду!");
+            }
+        }
+
+    }
+}
